249930f1a5d5de56ce08e8817a92b625cbc92fdd
Toussaint Etienne <tsst.etienne@gmail.com>
2016-01-17 22:04:09 +0100

    Move on

--- Python/UnionFind.py
+++ Python/UnionFind.py
@@ -6,7 +6,7 @@ class UnionFind():
     def __init__(self, tableau):
         self.pere = {element: element for element in tableau}
         self.nombreFils = {element: 1 for element in tableau}
-        self.tabWork = tableau
+        #self.tabWork = tableau
         self.ensRoot = set(tableau)
 
     def find(self, u, v):
